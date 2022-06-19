c00eb85f6d811d725e3c991c08f8af3a2c364c62
kevinchuchen <kevinchuchen@gmail.com>
2022-06-19 17:52:04 +0000

    Manages win-lose logic

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function computerPlay(){
 		computerOutput === 2 ? "scissors":"Undefined"
 	);
 }
+
+function playRound(playerSelection, computerSelection){
+	playerSelection = playerSelection.toLowerCase();
+	computerSelection = computerSelection.toLowerCase();
+	
+	if(playerSelection === "rock"){
+		return(
+			computerSelection === "scissors" ? "win":
+			computerSelection === "paper" ? "lose":
+			computerSelection === "rock" ? "draw":"undefined"
+		);
+	}
+	else if(playerSelection === "paper"){
+		return(
+			computerSelection === "scissors" ? "lose":
+			computerSelection === "paper" ? "draw":
+			computerSelection === "rock" ? "win":"undefined"
+		);
+	}
+	else if(playerSelection === "scissors"){
+		return(
+			computerSelection === "scissors" ? "draw":
+			computerSelection === "paper" ? "win":
+			computerSelection === "rock" ? "lose": "undefined"
+		);
+	}
+	else return("error"); //enter rock,paper, or scissors only
+}
